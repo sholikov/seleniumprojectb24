1b8598ac4d0cc8726daf478cb4ccf42eeabdd1e0
SHolikov <murodil@gmail.com>
2021-11-08 14:08:11 -0600

    testing with more than one digit

--- src/test/java/com/cybertek/practiceassignments/CalculatorAddTest2.java
+++ src/test/java/com/cybertek/practiceassignments/CalculatorAddTest2.java
@@ -3,6 +3,7 @@ package com.cybertek.practiceassignments;
 import com.cybertek.utils.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CalculatorAddTest2 {
     public static void main(String[] args) throws InterruptedException {
@@ -14,7 +15,7 @@ int expectedResult = 8
 3) Launch selenium, maximize and navigate to the url
          */
 
-        String appUrl ="https://www.calculator.net";
+        String appUrl = "https://www.calculator.net";
         int num1 = 128, num2 = 90;
         int expectedResult = 218;
 
@@ -25,12 +26,32 @@ int expectedResult = 8
         String numOne = "" + num1;
         String numTwo = "" + num2;
 
-        for(int i=0; i<numOne.length();i++){
+        for (int i = 0; i < numOne.length(); i++) {
 
-            driver.findElement(By.xpath("span[.='" + numOne.charAt(i)+"']")).click();
-           // Thread.sleep(1234);
-
+            driver.findElement(By.xpath("//span[.='" + numOne.charAt(i) + "']")).click();
+            // Thread.sleep(1234);
         }
+        Thread.sleep(1234);
+        WebElement plus = driver.findElement(By.xpath("//span[.='+']"));
+        plus.click();
+        Thread.sleep(1234);
+
+        for (int i = 0; i < numTwo.length(); i++) {
+
+            driver.findElement(By.xpath("//span[text()='" + numTwo.charAt(i) + "']")).click();
+        }
+        Thread.sleep(1234);
+        WebElement equalSign = driver.findElement(By.xpath("//span[.='=']"));
+        equalSign.click();
+
+        WebElement actualResult = driver.findElement(By.xpath("//div[@id='sciOutPut']"));
+        String result = actualResult.getText().trim();
+        System.out.println("result = " + result);
+
+        int actualOutput = Integer.parseInt(result);
+
+        System.out.println(actualOutput==expectedResult? "Test Passed" : "Test Failed");
+
 
     }
 }

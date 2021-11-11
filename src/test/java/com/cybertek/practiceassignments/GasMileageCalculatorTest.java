683d303fd56e5cca5c7a30f77482c8ebd5cfbb92
SHolikov <murodil@gmail.com>
2021-11-11 12:11:47 -0600

    GasMileageCalculatorTest practice

---
+++ src/test/java/com/cybertek/practiceassignments/GasMileageCalculatorTest.java
@@ -0,0 +1,66 @@
+package com.cybertek.practiceassignments;
+
+import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GasMileageCalculatorTest {
+     public static void main(String []args) throws InterruptedException {
+         String url  = "https://www.calculator.net";
+         WebDriver driver = WebDriverFactory.getDriver("chrome");
+         driver.manage().window().maximize();
+         driver.get(url);
+         WebElement searchBox = driver.findElement(By.cssSelector("input[type='text']"));
+         searchBox.sendKeys("gas mileage");
+         driver.findElement(By.xpath("//a[.='Gas Mileage Calculator']")).click();
+         //On Next page verify:
+         //o Title equals: “Gas Mileage Calculator”
+         //o “Gas Mileage Calculator” label is displayed
+         String currentTitle  = driver.getTitle();
+         System.out.println(currentTitle.equals("Gas Mileage Calculator")? "Title Matched": "Title didn't match");
+         
+         //String header = driver.findElement(By.tagName("h1")).getText();
+         //System.out.println("header = " + header);
+         WebElement header = driver.findElement(By.tagName("h1"));
+         System.out.println(header.isDisplayed());
+
+        // Locate, clear and type “7925” into “Current odometer” field
+
+/*
+         List<WebElement> inputFields = driver.findElements(By.cssSelector("input[type='text'"));
+
+         for (WebElement inputField : inputFields) {
+             inputField.clear();
+         }
+         */
+
+       //  Thread.sleep(1234);
+         WebElement curOdField = driver.findElement(By.id("uscodreading"));
+         curOdField.clear();
+         curOdField.sendKeys("7925");
+
+         WebElement prevOdField = driver.findElement(By.id("uspodreading"));
+         prevOdField.clear();
+         prevOdField.sendKeys("7550");
+
+         WebElement gasPutField = driver.findElement(By.id("usgasputin"));
+         gasPutField.clear();
+         gasPutField.sendKeys("16");
+
+         WebElement gasPriceField = driver.findElement(By.id("usgasprice"));
+         gasPriceField.clear();
+         gasPriceField.sendKeys("3.55");
+
+
+
+         driver.findElement(By.xpath("//input[@value='Calculate']")).click();
+         WebElement result = driver.findElement(By.tagName("h2"));
+         String calResult = result.getText();
+         System.out.println(calResult);
+         String digits = driver.findElement(By.xpath("//b[.='23.44 mpg']")).getText();
+         System.out.println(digits);
+
+
+     }
+}

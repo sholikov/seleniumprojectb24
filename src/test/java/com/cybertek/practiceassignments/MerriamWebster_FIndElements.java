2baa199668f61d9fd27ea2e280b2d48a2415990b
SHolikov <murodil@gmail.com>
2021-11-12 12:56:28 -0600

    MerriamWebster_FIndElements

---
+++ src/test/java/com/cybertek/practiceassignments/MerriamWebster_FIndElements.java
@@ -0,0 +1,34 @@
+package com.cybertek.practiceassignments;
+
+import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class MerriamWebster_FIndElements {
+    public static void main(String[] args) {
+
+        String url = "https://www.merriam-webster.com";
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get(url);
+
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        //
+        //  System.out.println(allLinks.size());
+        int missingText = 0;
+        int hasText = 0;
+
+        for (WebElement allLink : allLinks) {
+            if (!allLink.getText().isEmpty()) {
+                missingText++;
+            } else {
+                hasText++;
+            }
+
+        }
+        System.out.println(missingText + " are missing text \n"  + hasText + " have texts \n"  "There are total of " + allLinks);
+    }
+}

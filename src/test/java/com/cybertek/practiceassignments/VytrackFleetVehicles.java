8db302ea2cbbec6e09d1482b080203a69fd93bdd
SHolikov <murodil@gmail.com>
2021-12-09 11:15:59 -0600

    practice

--- src/test/java/com/cybertek/practiceassignments/VytrackFleetVehicles.java
+++ src/test/java/com/cybertek/practiceassignments/VytrackFleetVehicles.java
@@ -4,7 +4,8 @@ import com.cybertek.utils.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -14,35 +15,34 @@ public class VytrackFleetVehicles {
     String url = "https://qa2.vytrack.com/";
     WebDriver driver;
 
-    @BeforeClass
+
+    @BeforeMethod
     public void setUp() throws InterruptedException {
-        driver= WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(url);
-       WebElement username= driver.findElement(By.name("_username"));
-       username.sendKeys("user10");
-       WebElement password= driver.findElement(By.name("_password"));
-       password.sendKeys("UserUser123");
-       driver.findElement(By.id("_submit")).click();
+
+    }
+
+    @Test
+    public void locatingRefreshButtonTest() throws InterruptedException {
+
+        WebElement username = driver.findElement(By.name("_username"));
+        username.sendKeys("user10");
+        WebElement password = driver.findElement(By.name("_password"));
+        password.sendKeys("UserUser123");
+        driver.findElement(By.id("_submit")).click();
         driver.findElement(By.xpath("//li[@class='dropdown dropdown-level-1']//span")).click();
         Thread.sleep(1234);
         driver.findElement(By.xpath("//span[.='Vehicles']")).click();
         Thread.sleep(1234);
 
-        driver.findElement(By.xpath("//i[@class='fa-repeat']")).click();
 
-}
+        WebElement refreshButton = driver.findElement((By.xpath("//a[@title='Reset']/preceding-sibling::a[1]")));
+        Assert.assertTrue(refreshButton.isDisplayed(),"Refresh button is displayed on the left side od reset button");
 
-    @Test
-    public void locatingRefreshButtonTest(){
-        System.out.println("Testing");
-        WebElement refreshButton = driver.findElement(By.xpath("//i[.='fa-repeat']"));
-        refreshButton.click();
-        WebElement resetButton = driver.findElement(By.xpath("//i[.='fa-refresh"));
-        resetButton.click();
     }
-
 
 
 }
